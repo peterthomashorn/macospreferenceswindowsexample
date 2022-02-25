6c57541077bfa607951ae8cbd20b3c5c1d83a535
Peter Horn <7641832+peterthomashorn@users.noreply.github.com>
2022-02-25 21:24:28 +0100

    Initial commit.

---
+++ macospreferenceswindowexample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macospreferenceswindowexample
+//
+//  Created by Peter Thomas Horn on 25.02.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
